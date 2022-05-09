eb6616ffb1cc141dbe65993e089c7f923509bd9b
Kevin Aguilar <kevinrock_0103@htomail.com>
2022-05-09 17:44:33 -0500

    Ingreso de pagina

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,8 @@
-
-
+// Mouse
 for (let i=0;i<document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",playSound);
-    
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    /*
+    metodo con sentencia if 
     function playSound(){
         if (i==0){
             nombre="sounds/crash.mp3";
@@ -22,6 +22,64 @@ for (let i=0;i<document.querySelectorAll(".drum").length;i++){
         var audio=new Audio(nombre);
         audio.loop=false;
         audio.play();
-        document.querySelectorAll(".drum")[i].style.color="#E09510";
+        //Lo mismo que lo siguiente 
+        //document.querySelectorAll(".drum")[i].style.color="#E09510";
+        this.style.color="#E09510";
     }
+    */
+   // Otro mentodo con switch
+        let letra=this.innerHTML;
+        makeSound(letra);
+        buttonAnimation(letra);
+})}
+// Teclado
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+
+function makeSound(key){
+    let nombre; 
+    switch (key) {
+        case "w":
+            nombre="sounds/crash.mp3";
+            break;
+        case "a":
+            nombre="sounds/kick-bass.mp3";
+            break;
+        case "s":
+            nombre="sounds/snare.mp3";
+            break;
+        case "d":
+            nombre="sounds/tom-1.mp3";
+            break;
+        case "j":
+            nombre="sounds/tom-2.mp3";
+            break;
+        case "k":
+            nombre="sounds/tom-3.mp3";
+            break;
+        case "l":
+            nombre="sounds/tom-4.mp3";
+            break;
+        default:console.log(key);
+
+    }
+    var audio=new Audio(nombre);
+    audio.loop=false;
+    audio.play();
+}
+
+// Animación de botones generando un efecto de sombra 
+function buttonAnimation(currentKey){
+
+    let activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    // setTimeout genera un temporizador siendo elprimer elemento la funcion a ejecutar y despues
+    //el tiempo del efecto.
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100)
+
 }
